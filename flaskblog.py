e250985607abdfa645db947ecb4643f5dad0f169
Mark Kibara <markkibara2014@gmail.com>
2024-01-22 21:05:14 +0300

    Update proxy config

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
+# proxy fix
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+
+# proxy fix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1, x_prefix=1, x_for=1)
 
 @app.route("/")
 def hello_world():
