d07b0b28cc2d9d5d2503112e202aa832bb56c144
Mark Kibara <markkibara2014@gmail.com>
2024-01-24 10:39:38 +0300

    Add redirect capability

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for
 # proxy fix
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -14,6 +14,14 @@ def hello_world():
 @app.route("/hello")
 def api():
     return jsonify({"message": "Hello, World!"})
+
+@app.route("/hello/<name>")
+def api_name(name):
+    return jsonify({"message": f"Hello, {name}!"})
+
+@app.route("/hello/admin")
+def api_admin():
+    return redirect(url_for("api_name", name="Admin!"))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
